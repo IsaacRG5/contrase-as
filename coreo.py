0975075032d87a96987072da4d83a1bc7f890478
IsaacRG5 <isaacdavid1127@gmail.com>
2026-03-03 10:20:17 -0500

    todo listo

--- coreo.py
+++ coreo.py
@@ -1,1 +1,15 @@
-print("1")
+correo = input("Ingrese su correo: ")
+
+if correo.count("@") == 1:
+    poscion_arroba = correo.index("@")
+    punto_despues = False
+    for carecter in correo[poscion_arroba + 1:]:
+        if carecter == ".":
+            punto_despues = True
+
+    if punto_despues:
+        print("Valido")
+    else:
+        print("ERROR : El punto debe de estar despues del @")
+else:
+    print("ERROR: Debe de tener una sola @")
